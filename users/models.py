16ca0068602ee24a2ef266fbe4440947883c6cd5
Maiatskaia Natalia <mia@pop-os.localdomain>
2023-04-10 03:41:46 +0600

    Added email verification

--- users/models.py
+++ users/models.py
@@ -5,7 +5,12 @@ from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser):
-    email = models.EmailField(_("email_address"), unique=True,)
+    email = models.EmailField(
+        _('email address'),
+        unique=True,
+    )
+
+    email_verify = models.BooleanField(default=False)
     
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
